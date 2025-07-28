900a984a3568bd84d3468c21cb156ba44150c9c8
matisanabria <sanabria.mati29@gmail.com>
2025-07-28 20:01:23 -0300

    Created PinService that manages PIN

---
+++ src/main/java/app/barbman/onbarber/service/pin/PinService.java
@@ -0,0 +1,20 @@
+package app.barbman.onbarber.service.pin;
+
+import app.barbman.onbarber.repository.BarberoRepo;
+import app.barbman.onbarber.util.LoggerUtil;
+
+import java.util.logging.Logger;
+
+/**
+ * Servicio encargado de gestionar PIN's y de la autenticación
+ * mediante ellos
+ */
+public class PinService {
+    private static final Logger logger = LoggerUtil.getLogger(PinService.class);
+
+    public static void login(String PIN){
+
+    }
+
+
+}
