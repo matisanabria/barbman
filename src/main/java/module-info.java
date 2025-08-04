0a5c4958071740033e689db91352943fe332d25e
matisanabria <sanabria.mati29@gmail.com>
2025-08-04 17:17:16 -0300

    Added log4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,10 @@ module app.barbman.onbarber {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
-    requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires java.logging;
     requires java.sql;
+    requires org.apache.logging.log4j;
 
     opens app.barbman.onbarber to javafx.fxml;
     opens app.barbman.onbarber.controller to javafx.fxml;
